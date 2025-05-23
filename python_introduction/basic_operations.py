51bf78ffe6c5fcf2f85169bc3b8fa00e4e807503
Kevin Langat <langatkaykay@gmail.com>
2025-05-23 15:34:05 +0300

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1=10
+number2=5
+def addition(num1,num2):
+    print("Addition of "+str(num1)+" and "+str(num2)+" is "+ str(num1+num2))
+def difference(num1,num2):
+    print("Subtraction of "+str(num1)+" and "+str(num2)+" is "+ str(num1-num2))
+def product(num1,num2):
+    print("Multiplication of "+str(num1)+" and "+str(num2)+" is "+ str(num1*num2))
+
+addition(number1,number2)
+difference(number1,number2)
+product(number1,number2)
