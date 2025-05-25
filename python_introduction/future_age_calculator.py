b5fae0a03f1e0674a9771f4d943c9a9a6dd9a837
Kevin Langat <langatkaykay@gmail.com>
2025-05-25 11:06:58 +0300

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+current_age=input("How old are you? ")
+current_year=2023
+future_year=2050
+future_age=int(current_age) + (future_year - current_year)
+print(f"In {future_year}, you will be {future_age} years old")
